eb147a55121782f672918343076ba91df232f28a
11eph <ephraimiwuchukwu@.com>
2023-07-13 15:38:45 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: This is a program that prints various size of commputer.
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("Size of a char %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a int %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("size of a long int %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int %lu bytes(s)\n", (unsigned long)sizeof(d));
+printf("size of a float %lu bytes(s)\n", (unsigned long)sizeof(f));
+return (0);
+}
