4607d0da40aa755fd3657caa337ea79e69cd7b36
11eph <ephraimiwuchukwu@.com>
2023-07-13 11:00:27 -0700

    oxo c program 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description:This is a program that prints a phrase
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece art,\n");
+	return (0);
+}
+
