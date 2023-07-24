ae12e063b2296ec388737be48f73b1b6f6299b78
11eph <ephraimiwuchukwu@.com>
2023-07-24 03:51:57 -0700

    0x05 task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _puts - fuction that prints a string
+* @str: pointer to the string
+* Description: This is a program that prints a string.
+* Return: string and new line
+*/
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
